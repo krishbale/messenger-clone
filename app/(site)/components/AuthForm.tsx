6afde7562c7c3368ae55c20ae613918c56ed5dc2
Balkrishna pokharel <58511308+krishbale@users.noreply.github.com>
2023-05-17 12:54:42 +0000

    form created

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,8 +1,11 @@
 'use client';
 type Variant = 'LOGIN' | 'REGISTER'
+import { BsGithub, BsGoogle  } from 'react-icons/bs';
 import { useCallback, useState } from "react";
 import {FieldValues,SubmitHandler, useForm } from "react-hook-form";
 import Input from "@/app/components/Input";
+import Button from "@/app/components/Button";
+import AuthSocialButton from "./AuthSocialButton";
 export const AuthForm = () => {
     const [variant , setVariant] = useState<Variant>('LOGIN');
     const [isloading,setIsLoading] = useState(true);
@@ -65,9 +68,71 @@ export const AuthForm = () => {
             space-y-6
             "
             onSubmit={handleSubmit(onSubmit)} >
-            <Input />
+                {variant === 'REGISTER' && (
+            <Input
+             label="name"
+              errors={errors} 
+              id='name' 
+              register={register}
+               />
+               )}
+                <Input
+                 id='email' 
+             label="Email Address"
+              errors={errors} 
+              type={register}
+             
+              register={register}
+               />
+                <Input
+                 id='password' 
+             label="Password"
+              errors={errors} 
+              type={register}
+              register={register}
+               />
+               <Button
+               disabled={isloading}
+               fullWidth
+               type="submit"
+               >{variant ==='LOGIN' ? 'Sign in ':'Register'}</Button>
+           
             </form>
+            <div className="mt-6">
+          <div className="relative">
+            <div 
+              className="
+                absolute 
+                inset-0 
+                flex 
+                items-center
+              "
+            >
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+                        <div className="mt-6 flex gap-2">
+            <AuthSocialButton 
+              icon={BsGithub} 
+             
+            />
+            <AuthSocialButton 
+              icon={BsGoogle} 
+             
+            />
+          </div>
         </div>
-    </div>
+
+                        
+                        
+                    </div>
+                </div>
+            
+   
   )
 }
